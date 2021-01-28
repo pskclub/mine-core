3ab8e8eb3e067e274698afb27b500e2e49c0a8f7
PskCluB <passakon_p@hotmail.com>
2021-01-29 02:56:23 +0700

    Add gen and compare password

--- utils/crypto.go
+++ utils/crypto.go
@@ -6,6 +6,8 @@ import (
 	"encoding/base64"
 	"encoding/hex"
 	"fmt"
+	"golang.org/x/crypto/bcrypt"
+	"log"
 )
 
 func NewSha256(str string) string {
@@ -50,4 +52,32 @@ func Base64Decode(_base64 string) (string, error) {
 
 func Base64Encode(str string) string {
 	return base64.StdEncoding.EncodeToString(StringToBytes(str))
+}
+
+func HashAndSalt(pwd string) string {
+	// Use GenerateFromPassword to hash & salt pwd.
+	// MinCost is just an integer constant provided by the bcrypt
+	// package along with DefaultCost & MaxCost.
+	// The cost can be any value you want provided it isn't lower
+	// than the MinCost (4)
+	hash, err := bcrypt.GenerateFromPassword([]byte(pwd), bcrypt.MinCost)
+	if err != nil {
+		log.Println(err)
+	}
+	// GenerateFromPassword returns a byte slice so we need to
+	// convert the bytes to a string and return it
+	return string(hash)
+}
+
+func ComparePasswords(hashedPwd string, plainPwd string) bool {
+	// Since we'll be getting the hashed password from the DB it
+	// will be a string so we'll need to convert it to a byte slice
+	byteHash := []byte(hashedPwd)
+	err := bcrypt.CompareHashAndPassword(byteHash, []byte(plainPwd))
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+
+	return true
 }
