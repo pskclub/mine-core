ab9aad1ab235253bf0c09f82226795c6d83bf665
PskCluB <passakon_p@hotmail.com>
2021-02-27 18:33:01 +0700

    Add cache middleware

--- http_middleware_with_cache.go
+++ http_middleware_with_cache.go
@@ -7,7 +7,7 @@ import (
 	"net/http"
 )
 
-func WithCache(key string) echo.MiddlewareFunc {
+func HTTPWithCache(key string) echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
 			cc := c.(IHTTPContext)
