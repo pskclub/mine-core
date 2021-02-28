3059f2094d11b830f727536f87d94c31beaf3446
PskCluB <passakon_p@hotmail.com>
2021-02-28 17:59:31 +0700

    Add search els

--- els.go
+++ els.go
@@ -5,8 +5,11 @@ import (
 	"encoding/json"
 	"github.com/elastic/go-elasticsearch/v7"
 	"github.com/elastic/go-elasticsearch/v7/esapi"
+	"github.com/go-errors/errors"
 	"github.com/pskclub/mine-core/utils"
+	"github.com/tidwall/gjson"
 	"io"
+	"io/ioutil"
 )
 
 type ELS struct {
@@ -21,8 +24,10 @@ type els struct {
 
 type IELS interface {
 	Client() *elasticsearch.Client
-	CreateIndex(name string, body interface{}, options *ELSCreateIndexOptions) error
-	Create(dest interface{}, index string, id string, body interface{}, options *ELSCreateIndexOptions) (*esapi.Response, error)
+	CreateIndex(name string, body map[string]interface{}, options *ELSCreateIndexOptions) error
+	Create(dest interface{}, index string, id string, body map[string]interface{}, options *ELSCreateIndexOptions) (*esapi.Response, error)
+	Upsert(dest interface{}, index string, id string, body map[string]interface{}, options *ELSCreateIndexOptions) (*esapi.Response, error)
+	SearchPagination(dest interface{}, index string, body map[string]interface{}, pageOptions *PageOptions, opts *ELSCreateSearchOptions) (*PageResponse, error)
 }
 
 func (e ELS) Connect() (IELS, error) {
@@ -53,16 +58,20 @@ func (e els) Client() *elasticsearch.Client {
 type ELSCreateIndexOptions struct {
 }
 
-func (e els) CreateIndex(name string, body interface{}, options *ELSCreateIndexOptions) error {
+func (e els) CreateIndex(name string, body map[string]interface{}, options *ELSCreateIndexOptions) error {
 	_, err := e.Client().Indices.Create(name)
 	if err != nil {
 		return err
 	}
 
 	if body != nil {
-		_, err = e.Client().Indices.PutMapping(e.interfaceToReader(body), e.Client().Indices.PutMapping.WithIndex(name))
+		res, err := e.Client().Indices.PutMapping(e.interfaceToReader(body), e.Client().Indices.PutMapping.WithIndex(name))
 		if err != nil {
 			return err
+		}
+
+		if res.IsError() {
+			return errors.New(res.String())
 		}
 	}
 
@@ -72,10 +81,65 @@ func (e els) CreateIndex(name string, body interface{}, options *ELSCreateIndexO
 type ELSCreateCreateOptions struct {
 }
 
-func (e els) Create(dest interface{}, index string, id string, body interface{}, options *ELSCreateIndexOptions) (*esapi.Response, error) {
+func (e els) Create(dest interface{}, index string, id string, body map[string]interface{}, options *ELSCreateIndexOptions) (*esapi.Response, error) {
 	res, err := e.Client().Create(index, id, e.interfaceToReader(body))
 	if err != nil {
 		return nil, err
+	}
+
+	if res.IsError() {
+		return nil, errors.New(res.String())
+	}
+	_ = utils.JSONParse(utils.StringToBytes(res.String()), dest)
+
+	return res, nil
+}
+
+type ELSCreateSearchOptions struct {
+}
+
+func (e els) getFrom(pageOptions *PageOptions) int64 {
+	return pageOptions.Limit * (pageOptions.Page - 1)
+}
+
+func (e els) SearchPagination(dest interface{}, index string, body map[string]interface{}, pageOptions *PageOptions, opts *ELSCreateSearchOptions) (*PageResponse, error) {
+	body["from"] = e.getFrom(pageOptions)
+	body["size"] = pageOptions.Limit
+	res, err := e.Client().Search(e.Client().Search.WithBody(bytes.NewBufferString(utils.JSONToString(body))), e.Client().Search.WithIndex(index))
+	if err != nil {
+		return nil, err
+	}
+
+	if res.IsError() {
+		return nil, errors.New(res.String())
+	}
+
+	resByte, _ := ioutil.ReadAll(res.Body)
+	result := gjson.GetManyBytes(resByte, "hits.hits.#._source", "hits.total.value")
+	_ = json.Unmarshal([]byte(result[0].Raw), dest)
+
+	return &PageResponse{
+		Total:   result[1].Int(),
+		Limit:   pageOptions.Limit,
+		Count:   int64(len(dest.([]interface{}))),
+		Page:    pageOptions.Page,
+		Q:       pageOptions.Q,
+		OrderBy: pageOptions.OrderBy,
+	}, nil
+}
+
+func (e els) Upsert(dest interface{}, index string, id string, body map[string]interface{}, options *ELSCreateIndexOptions) (*esapi.Response, error) {
+	newBody := Map{
+		"doc":           body,
+		"doc_as_upsert": true,
+	}
+	res, err := e.Client().Update(index, id, e.interfaceToReader(newBody))
+	if err != nil {
+		return nil, err
+	}
+
+	if res.IsError() {
+		return nil, errors.New(res.String())
 	}
 
 	_ = utils.JSONParse(utils.StringToBytes(res.String()), dest)
--- go.mod
+++ go.mod
@@ -22,6 +22,7 @@ require (
 	github.com/stretchr/testify v1.6.1
 	github.com/teris-io/shortid v0.0.0-20201117134242-e59966efd125
 	github.com/thedevsaddam/gojsonq/v2 v2.5.2
+	github.com/tidwall/gjson v1.6.8 // indirect
 	go.mongodb.org/mongo-driver v1.4.6
 	golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a
 	gopkg.in/asaskevich/govalidator.v9 v9.0.0-20180315120708-ccb8e960c48f
